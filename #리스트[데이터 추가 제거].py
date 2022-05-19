613f1295e05b30d0cd3344778450dff4cadf65b4
SonSeolHui <alck97@naver.com>
2022-05-20 00:32:33 +0900

    최초 작성

---
+++ #리스트[데이터 추가 제거].py
@@ -0,0 +1,17 @@
+#리스트[2]
+#리스트에 데이터 추가하기/append 사용
+
+A = ['apple', 'cake', 'cheese']
+A.append('goguma')
+print(A)
+
+#리스트에서 데이터 제거하기
+#del과 remove사용
+
+del A(-1)
+print(A)
+
+A.remove('apple')
+print(A)
+
+# if same str in list, be deleted front things
